40b956c6149b782d4578a1188004aba497c15ad3
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-15 06:39:39 +0300

    Add class ListTemplate with ul constructor

--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -5,3 +5,12 @@ interface DOMList {
   clear(): void,
   render(fullList: FullList): void,
 }
+
+export default class ListTemplate implements DOMList {
+
+  ul: HTMLUListElement
+  
+  private constructor() {
+    this.ul = document.getElementById("listItems") as HTMLUListElement
+  }
+}
